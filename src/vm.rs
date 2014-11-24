7f2074ab0c9b6b14ccd7fb8b360923fb5976591a
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2014-11-24 10:12:52 -0800

    Namespaced enums - Rust breaking change

--- src/vm.rs
+++ src/vm.rs
@@ -1,15 +1,15 @@
-pub fn perin_version() -> f32{
-	0.1
-}
-
 #[deriving(FromPrimitive)]
-enum Instruction {
+pub enum Instruction {
 	INTEGER = 0x00,
 	STRING = 0x01,
 	ADD = 0x02,
 	SHOWINTEGER = 0x0A,
 	SHOWVERSION = 0x0E,
 	EXITVM = 0x0F
+}
+
+pub fn perin_version() -> f32{
+	0.1
 }
 
 pub struct PerinVM{
@@ -39,25 +39,25 @@ impl PerinVM{
 				},
 				Some(bc) => {
 					match bc{
-						INTEGER => {
+						Instruction::INTEGER => {
 							self.push=true;
 						},
-						ADD => {
+						Instruction::ADD => {
 							let a=self.pop() as int;
 							let b=self.pop() as int;
 							let c=a+b;
 							self.push(c as u8);
 						},
-						SHOWINTEGER => {
+						Instruction::SHOWINTEGER => {
 							println!("Integer value {}",self.pop() as int);
 						},
-						SHOWVERSION => {
+						Instruction::SHOWVERSION => {
 							println!("PerinVM v0.1.0");
 						},
-						EXITVM => {
+						Instruction::EXITVM => {
 							println!("Exit VM");
 						},
-						STRING => {
+						Instruction::STRING => {
 							println!("Unsupported instruction 'STRING' ");
 						}
 					}
