96aed5453a58e660417611a76255af5824721ec1
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2014-11-24 10:13:12 -0800

    Doing a more complex math operation - 2+3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-
 mod vm;
 
 fn main(){
-	let bytecode = "\x0E\x00\x02\x00\x02\x02\x0A";
+	let bytecode = "\x0E\x00\x03\x00\x02\x02\x0A";
 	println!("Perin v{}",vm::perin_version());
 	println!("Perin VM executes FlopFlip bytecode");
 	
